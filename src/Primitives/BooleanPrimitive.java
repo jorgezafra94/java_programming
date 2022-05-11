38086791380f8968c056824b41156e60bf158ed5
jorgezafra94 <jorgezafra94@hotmail.com>
2022-05-11 18:02:55 -0500

    adding boolean primitive knowledge

---
+++ src/Primitives/BooleanPrimitive.java
@@ -0,0 +1,22 @@
+package Primitives;
+
+public class BooleanPrimitive {
+    /*
+    * estos son valore logico o true o false, se utilizan para flujos de control y toma de decisiones a partir de comparaciones
+    */
+
+    boolean x = true;
+    boolean y = false;
+    boolean z = 23 > 12;
+
+    /*
+    * aqui se pueden usar:
+    * == (igualdad de objeto o para primitivos es valor)
+    * != (diferente objeto o para primitivos es valor)
+    * > (mayor)
+    * < (menor)
+    * >= (mayor o igual)
+    * <= (menor o igual)
+    * .equals (es un metodo que tienen todos los objetos y este evalua valor mas no que tengan la misma direccion de memmoria)
+    */
+}
